ff741ea4438c5ff263c24f3f0830563618432f6e
mohamedabd-elnasser813 <anklmohamed813@gmail.com>
2025-08-17 22:43:03 +0300

    Update SocialMedia.jsx

--- src/Compomet/SocialMedia/SocialMedia.jsx
+++ src/Compomet/SocialMedia/SocialMedia.jsx
@@ -5,8 +5,8 @@ export default function SocialMedia({ className }) {
   return (
     <>
 
-      <Link to="https://github.com/OmarMostafa6103" target='_blank'><i className={`dark:text-white text-black ${className} fa-brands fa-github`}></i></Link>
-      <Link to="https://www.linkedin.com/feed/"><i className={`fa-brands fa-linkedin dark:text-white text-black ${className}`}></i></Link>
+      <Link to="https://github.com/mohamedabd-elnasser813" target='_blank'><i className={`dark:text-white text-black ${className} fa-brands fa-github`}></i></Link>
+      <Link to="https://www.linkedin.com/in/mohamed-abd-elnasser-a81b39240/"><i className={`fa-brands fa-linkedin dark:text-white text-black ${className}`}></i></Link>
     </>
   );
 }
